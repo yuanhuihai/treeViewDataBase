8c3bb79d7e6801b50a77e060a93eba39fa093e04
yuanhuihai <yuanhuihai@sina.com>
2021-05-08 22:50:48 +0800

    20210508

--- treeViewDataBase/Form1.Designer.cs
+++ treeViewDataBase/Form1.Designer.cs
@@ -28,13 +28,173 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tree_Role = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fname = new System.Windows.Forms.TextBox();
+            this.cname = new System.Windows.Forms.TextBox();
+            this.flevel = new System.Windows.Forms.TextBox();
+            this.clevel = new System.Windows.Forms.TextBox();
+            this.baocun = new System.Windows.Forms.Button();
+            this.baocun2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.choseName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tree_Role
+            // 
+            this.tree_Role.Location = new System.Drawing.Point(12, 12);
+            this.tree_Role.Name = "tree_Role";
+            this.tree_Role.Size = new System.Drawing.Size(311, 552);
+            this.tree_Role.TabIndex = 0;
+            this.tree_Role.DoubleClick += new System.EventHandler(this.tree_Role_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(333, 287);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "添加子节点";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(333, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 30);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "添加父节点";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(566, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 30);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "名称";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(760, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 30);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "级别";
+            // 
+            // fname
+            // 
+            this.fname.Location = new System.Drawing.Point(499, 149);
+            this.fname.Name = "fname";
+            this.fname.Size = new System.Drawing.Size(206, 42);
+            this.fname.TabIndex = 6;
+            // 
+            // cname
+            // 
+            this.cname.Location = new System.Drawing.Point(499, 287);
+            this.cname.Name = "cname";
+            this.cname.Size = new System.Drawing.Size(206, 42);
+            this.cname.TabIndex = 7;
+            // 
+            // flevel
+            // 
+            this.flevel.Location = new System.Drawing.Point(742, 149);
+            this.flevel.Name = "flevel";
+            this.flevel.Size = new System.Drawing.Size(91, 42);
+            this.flevel.TabIndex = 8;
+            // 
+            // clevel
+            // 
+            this.clevel.Location = new System.Drawing.Point(742, 287);
+            this.clevel.Name = "clevel";
+            this.clevel.Size = new System.Drawing.Size(91, 42);
+            this.clevel.TabIndex = 9;
+            // 
+            // baocun
+            // 
+            this.baocun.Location = new System.Drawing.Point(863, 142);
+            this.baocun.Name = "baocun";
+            this.baocun.Size = new System.Drawing.Size(114, 51);
+            this.baocun.TabIndex = 10;
+            this.baocun.Text = "保存";
+            this.baocun.UseVisualStyleBackColor = true;
+            this.baocun.Click += new System.EventHandler(this.baocun_Click);
+            // 
+            // baocun2
+            // 
+            this.baocun2.Location = new System.Drawing.Point(863, 287);
+            this.baocun2.Name = "baocun2";
+            this.baocun2.Size = new System.Drawing.Size(116, 51);
+            this.baocun2.TabIndex = 11;
+            this.baocun2.Text = "保存";
+            this.baocun2.UseVisualStyleBackColor = true;
+            this.baocun2.Click += new System.EventHandler(this.baocun2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(333, 487);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(223, 30);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "选中的节点名称";
+            // 
+            // choseName
+            // 
+            this.choseName.Location = new System.Drawing.Point(571, 484);
+            this.choseName.Name = "choseName";
+            this.choseName.Size = new System.Drawing.Size(206, 42);
+            this.choseName.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1020, 576);
+            this.Controls.Add(this.choseName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.baocun2);
+            this.Controls.Add(this.baocun);
+            this.Controls.Add(this.clevel);
+            this.Controls.Add(this.flevel);
+            this.Controls.Add(this.cname);
+            this.Controls.Add(this.fname);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tree_Role);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "树形菜单-从数据库加载数据";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView tree_Role;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox fname;
+        private System.Windows.Forms.TextBox cname;
+        private System.Windows.Forms.TextBox flevel;
+        private System.Windows.Forms.TextBox clevel;
+        private System.Windows.Forms.Button baocun;
+        private System.Windows.Forms.Button baocun2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox choseName;
     }
 }
 
--- treeViewDataBase/Form1.cs
+++ treeViewDataBase/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace treeViewDataBase
 {
@@ -14,6 +15,117 @@ namespace treeViewDataBase
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //窗体加载
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            InitModuleTree(getData());
+
+        }
+        //从数据库获取数据
+        public DataTable getData()
+        {
+            //数据库连接
+            SQLiteConnection myCon;
+            myCon = new SQLiteConnection("Data Source=tree.sqlite;Version=3;");
+            myCon.Open();
+            //选择所有数据
+            SQLiteDataAdapter mAdapter = new SQLiteDataAdapter("select * from tree", myCon);
+            DataTable dt = new DataTable();
+            mAdapter.Fill(dt);
+          //关闭数据库
+            myCon.Close();
+            return dt;
+        }
+        //绑定TrreView
+        private void InitModuleTree(DataTable dt)
+        {
+            //清空treeview上所有节点
+            this.tree_Role.Nodes.Clear();
+            int[] gen = new int[dt.Rows.Count]; //用于存储父节点Tag
+            int[] zi = new int[dt.Rows.Count];  //用于存储子节点Tag
+            for (int i = 0; i < gen.Length; i++)
+            {
+                string zhi = dt.Rows[i][1].ToString();//获取节点Tag值   eg：1-2
+                if (zhi.Length > 1)   //表示是子节点   eg：1-2
+                {
+                    gen[i] = int.Parse(zhi.Substring(0, zhi.IndexOf('-')));
+                    zi[i] = int.Parse(zhi.Substring(zhi.IndexOf('-') + 1));
+                }
+                else    //表示是根节点   eg：2
+                {
+                    //将所有父节点加到treeview上
+                    zi[i] = int.Parse(zhi);
+                    TreeNode nodeParent = new TreeNode();
+                    nodeParent.Tag = (zi[i]).ToString();
+                    nodeParent.Text = dt.Rows[i][0].ToString();//填充父节点名称
+                    tree_Role.Nodes.Add(nodeParent);
+                }
+            }
+            bindChildNote(dt, gen, zi);
+        }
+        //绑定子节点
+        private void bindChildNote(DataTable dt, int[] gen, int[] zi)
+        {
+            for (int i = 0; i < gen.Length; i++)
+            {
+                if (gen[i] != 0 && zi[i] != 0)        //便利所有节点，找到所有子节点
+                {
+                    TreeNode childNode = new TreeNode();
+                    foreach (TreeNode item in tree_Role.Nodes)   //便历treeview上所有父节点
+                    {
+                        if (item.Tag.ToString() == gen[i].ToString())  //找到当前子节点的父节点
+                        {
+                            childNode.Tag = zi[i].ToString();
+                            childNode.Text = dt.Rows[i][0].ToString();//填充子节点名称
+                            item.Nodes.Add(childNode);
+                        }
+                    }
+                }
+            }
+            tree_Role.ExpandAll();      //展开整棵树
+        } 
+        //保存父节点
+        private void baocun_Click(object sender, EventArgs e)
+        {
+            //数据库连接
+            SQLiteConnection myCon;
+            myCon = new SQLiteConnection("Data Source=tree.sqlite;Version=3;");
+            myCon.Open();
+
+            string sql = "insert into tree values ('" + fname.Text + "', '" + flevel.Text + "')";
+            SQLiteCommand command = new SQLiteCommand(sql, myCon);
+            command.ExecuteNonQuery();
+       
+            myCon.Close();
+            InitModuleTree(getData());
+        }
+        //保存子节点
+        private void baocun2_Click(object sender, EventArgs e)
+        {
+            //数据库连接
+            SQLiteConnection myCon;
+            myCon = new SQLiteConnection("Data Source=tree.sqlite;Version=3;");
+            myCon.Open();
+
+            string sql = "insert into tree values ('" + cname.Text + "', '" + clevel.Text + "')";
+            SQLiteCommand command = new SQLiteCommand(sql, myCon);
+            command.ExecuteNonQuery();
+      
+            myCon.Close();
+            InitModuleTree(getData());
+        }
+
+     
+
+      
+
+        private void tree_Role_DoubleClick(object sender, EventArgs e)
+        {
+        
+            choseName.Text = tree_Role.SelectedNode.Text;
         }
     }
 }
